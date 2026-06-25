a5aae6555af9aee824567494409ffec26a6a16be
Hareesh Puthalath <hareeshp@nvidia.com>
2026-06-25 11:18:03 +0200

    feat: add option to disable SNAT on default gateway routers
    Add a new --disable-snat-gateway-routers CLI flag
    (env: OVN_DISABLE_SNAT_GATEWAY_ROUTERS) that disables default
    cluster-subnet SNAT rules on gateway routers for the default network.
    
    When enabled, OVN will not install SNAT rules that masquerade pod
    traffic behind the node's external IP on gateway routers. This is
    useful in deployments where pod CIDRs are externally routable (e.g.,
    BGP-advertised) and SNAT would break return path routing.
    
    Changes:
    - config: add DisableSNATGatewayRouters bool to GatewayConfig and
      register the --disable-snat-gateway-routers CLI flag
    - gateway: skip NAT rule installation in updateGWRouterNAT when the
      flag is set on the default network; stale NAT entries are still
      cleaned up on upgrade
    - ovnkube.sh: wire OVN_DISABLE_SNAT_GATEWAY_ROUTERS into ovn-master,
      ovnkube-controller, ovnkube-controller-with-node, ovn-cluster-manager
      and ovn-node modes
    - tests: add gateway_test case verifying no SNAT rules are created when
      the flag is set; update master_test skipSnat logic to cover the new flag
    
    Signed-off-by: Hareesh Puthalath <hareeshp@nvidia.com>
    (cherry picked from commit d8e8aa51055b7aa77d4fb8d46ead11878e88d49d)

--- go-controller/pkg/ovn/master_test.go
+++ go-controller/pkg/ovn/master_test.go
@@ -1204,7 +1204,8 @@ var _ = ginkgo.Describe("Default network controller operations", func() {
 				err = oc.syncNodeGateway(testNode)
 				gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-				skipSnat := config.Gateway.DisableSNATMultipleGWs && !oc.GetNetInfo().IsPrimaryNetwork()
+				skipSnat := (config.Gateway.DisableSNATGatewayRouters && oc.GetNetInfo().IsDefault()) ||
+					(config.Gateway.DisableSNATMultipleGWs && !oc.GetNetInfo().IsPrimaryNetwork())
 				var clusterSubnets []*net.IPNet
 				for _, clusterSubnet := range config.Default.ClusterSubnets {
 					clusterSubnets = append(clusterSubnets, clusterSubnet.CIDR)
@@ -1262,6 +1263,14 @@ var _ = ginkgo.Describe("Default network controller operations", func() {
 			"When DisableSNATMultipleGWs is false",
 			func(*DefaultNetworkController) error {
 				config.Gateway.DisableSNATMultipleGWs = false
+				return nil
+			},
+			newNodeSNAT("stale-nodeNAT-UUID-4", "10.0.0.3", "172.16.16.3"), // won't be deleted on this node but will be deleted on the node whose IP is 172.16.16.3 since this pod belongs to this node
+		),
+		ginkgo.Entry(
+			"When DisableSNATGatewayRouters is true",
+			func(*DefaultNetworkController) error {
+				config.Gateway.DisableSNATGatewayRouters = true
 				return nil
 			},
 			newNodeSNAT("stale-nodeNAT-UUID-4", "10.0.0.3", "172.16.16.3"), // won't be deleted on this node but will be deleted on the node whose IP is 172.16.16.3 since this pod belongs to this node
